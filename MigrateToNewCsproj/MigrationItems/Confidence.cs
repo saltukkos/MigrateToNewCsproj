2fcbd07845de17e37fcefcf44878c543ee6cfa80
saltukkos <saltukkos@gmail.com>
2018-08-11 14:22:15 +0600

    Add description for confidence levels

--- MigrateToNewCsproj/MigrationItems/Confidence.cs
+++ MigrateToNewCsproj/MigrationItems/Confidence.cs
@@ -2,8 +2,31 @@
 {
     public enum Confidence
     {
+        /// <summary>
+        /// Means that migration is correctly defined
+        /// and knows what it's doing
+        /// </summary>
         Rule,
-        Assumption,
+
+        /// <summary>
+        /// Means that migration is not defined and
+        /// current action is assumption, but it will
+        /// not lost any data, so in worth case you will
+        /// get error while loading project and will be able
+        /// to fix it manually 
+        /// </summary>
+        SafeAssumption,
+
+        /// <summary>
+        /// Means that migration is not defined and
+        /// current action is assumption that can lost data
+        /// and broke the build
+        /// </summary>
+        DangerAssumption,
+
+        /// <summary>
+        /// Migration is not possible without user intervention
+        /// </summary>
         UserInterventionRequired
     }
 }
