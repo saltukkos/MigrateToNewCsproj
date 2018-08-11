f1c3172eb71f2124cd300248f77e8415455bc7ac
saltukkos <saltukkos@gmail.com>
2018-08-11 14:55:29 +0600

    create default migrators

---
+++ MigrateToNewCsproj/ProjectMigrator/DefaultCopyMigrationAction.cs
@@ -0,0 +1,28 @@
+﻿using System.Xml.Linq;
+using JetBrains.Annotations;
+using MigrationItems;
+using Saltuk.Utils.Validation;
+
+namespace ProjectMigrator
+{
+    public class DefaultCopyMigrationAction : IMigrationAction
+    {
+        [NotNull]
+        private readonly XElement _originalElement;
+
+        public DefaultCopyMigrationAction([NotNull] XElement originalElement)
+        {
+            ThrowIf.Argument.IsNull(originalElement, nameof(originalElement));
+            _originalElement = originalElement;
+        }
+
+        public Confidence Confidence => Confidence.SafeAssumption;
+        
+        public UserInputRequest RequiredUserInput { get; } = new UserInputRequest();
+        
+        public XElement GetElement(UserInput userInput)
+        {
+            return _originalElement;
+        }
+    }
+}
---
+++ MigrateToNewCsproj/ProjectMigrator/DefaultDeleteMigrationAction.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml.Linq;
+using MigrationItems;
+
+namespace ProjectMigrator
+{
+    public class DefaultDeleteMigrationAction : IMigrationAction
+    {
+        public Confidence Confidence => Confidence.DangerAssumption;
+        
+        public UserInputRequest RequiredUserInput { get; } = new UserInputRequest();
+
+        public XElement GetElement(UserInput userInput)
+        {
+            return null;
+        }
+    }
+}
